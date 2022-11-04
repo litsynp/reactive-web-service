a8069bab9327574b0be2e06f901428905c95fb67
litsynp <nocte55is@gmail.com>
2022-11-05 01:58:07 +0900

    Add reactive REST API and test

---
+++ README.md
@@ -0,0 +1,28 @@
+# reactive-web-service
+
+## About
+
+This repository follows guides from Spring.io to build a reactive REST service.
+
+## Prerequisites
+
+- Java 11
+
+## Run
+
+### Option 1. Run with Gradle
+
+```bash
+$ ./gradlew bootRun
+```
+
+### Option 2. Build an executable JAR
+
+```bash
+$ ./gradlew build
+$ java -jar build/libs/reactive-web-service-0.1.0.jar
+```
+
+## References
+
+[Spring.io - Getting Started | Reactive REST Service](https://spring.io/guides/gs/reactive-rest-service/)
--- src/main/java/com/litsynp/app/Application.java
+++ src/main/java/com/litsynp/app/Application.java
@@ -1,13 +1,17 @@
 package com.litsynp.app;
 
+import com.litsynp.app.hello.GreetingClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+        GreetingClient greetingClient = context.getBean(GreetingClient.class);
+        System.out.println(">> message = " + greetingClient.getMessage().block());
+    }
 
 }
